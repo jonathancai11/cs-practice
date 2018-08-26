53ccce3379600f373ae56735232ccd2bbd070ea6
Jonathan Cai <caimjonathan@gmail.com>
2018-08-26 16:30:23 -0500

    Redefine ch8p1, dif int.

--- ctci/ch8-recursion-dp/p1.py
+++ ctci/ch8-recursion-dp/p1.py
@@ -3,20 +3,17 @@
 
 def numsteps(n):
     """
-    Recursive, returns lowest number of steps needed for a child to step up n steps.
+    Recursive, returns the number of paths for a child to step up n steps.
     Child can step 1, 2 or 3 steps at a time.
     """
     # Base Cases
-    if n == 0:
-        return 0
-    if n == 1 or n == 2 or n == 3:
-        return 1
-    return min(numsteps(n-1), numsteps(n-2), numsteps(n-3)) + 1
+    if n == 0 or n == 1 or n == 2:
+        return n
+    return numsteps(n-1) + numsteps(n-2) + numsteps(n-3) + 3
 
 
 # TESTING
-print(numsteps(5), "Expected: 2")
-print(numsteps(10), "Expected: 4")
+print(numsteps(5), "Expected: idk")
+print(numsteps(10), "Expected: idk")
 print(numsteps(0), "Expected: 0")
-print(numsteps(20), "Expected: 7")
 
